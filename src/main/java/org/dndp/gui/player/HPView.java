aa0bbc80076057811615685f811b069c978b6abe
bambucha <bambucha14@gmail.com>
2011-12-21 19:48:21 +0100

    Dodanie widoku punktów życia.

---
+++ src/main/java/org/dndp/gui/player/HPView.java
@@ -0,0 +1,19 @@
+package org.dndp.gui.player;
+import java.net.URL;
+
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.BoxPane;
+
+public class HPView extends BoxPane implements Bindable
+{
+
+	@Override
+	public void initialize(Map<String, Object> namespace, URL location,
+			Resources resources)
+	{
+
+	}
+
+}
