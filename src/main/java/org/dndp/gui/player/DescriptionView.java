da9ff29905948e262c425308246b2c38dcdd9208
bambucha <bambucha14@gmail.com>
2011-12-21 04:34:01 +0100

    dodnie stuba widoku

--- src/main/java/org/dndp/gui/player/DescriptionView.java
+++ src/main/java/org/dndp/gui/player/DescriptionView.java
@@ -6,10 +6,19 @@ import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
 import org.apache.pivot.wtk.BoxPane;
+import org.apache.pivot.wtk.Orientation;
+import org.apache.pivot.wtk.Panel;
 
-public class AbilitiesView extends BoxPane implements Bindable
+public class DescriptionView extends BoxPane implements Bindable
 {
+	public DescriptionView()
+	{
+		setOrientation(Orientation.VERTICAL);
+		getStyles().put("fill", true);
+		getStyles().put("padding", 4);
+	}
 
+	@Override
 	public void initialize(Map<String, Object> namespace, URL location,
 			Resources resources)
 	{
