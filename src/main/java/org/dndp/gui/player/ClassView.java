898aa06ab98a5bbc6abfd9b1d534511cb39632cd
bambucha <bambucha14@gmail.com>
2011-12-21 05:37:42 +0100

    Dodanie stuba dla Widoku Klas postaci.

---
+++ src/main/java/org/dndp/gui/player/ClassView.java
@@ -0,0 +1,20 @@
+package org.dndp.gui.player;
+
+import java.net.URL;
+
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.TablePane;
+
+public class ClassView extends TablePane implements Bindable
+{
+
+	@Override
+	public void initialize(Map<String, Object> namespace, URL location,
+			Resources resources)
+	{
+		// TODO implementacja
+	}
+
+}
