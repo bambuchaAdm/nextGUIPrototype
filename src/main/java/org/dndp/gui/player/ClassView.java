f059c41d7647136e2d2283a8a373075622eefb3b
bambucha <bambucha14@gmail.com>
2011-12-21 05:49:41 +0100

    Dlasze zabawy. Dodanie listy oraz miejsca na wyświetlanie dokładnych informacji o klasie.

--- src/main/java/org/dndp/gui/player/ClassView.java
+++ src/main/java/org/dndp/gui/player/ClassView.java
@@ -5,10 +5,18 @@ import java.net.URL;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.BoxPane;
+import org.apache.pivot.wtk.GridPane;
+import org.apache.pivot.wtk.Orientation;
 import org.apache.pivot.wtk.TablePane;
 
-public class ClassView extends TablePane implements Bindable
+public class ClassView extends GridPane implements Bindable
 {
+	public ClassView()
+	{
+		getStyles().put("padding", 3);
+		getStyles().put("fill", true);
+	}
 
 	@Override
 	public void initialize(Map<String, Object> namespace, URL location,
